f38faa79b4f298ca9f41cc5c719649c1d4627fea
Sukhpreetsanghu <sanghusukhpreet@gmail.com>
2026-06-10 13:17:31 +0530

    Completed

--- Assignment7.py
+++ Assignment7.py
@@ -1,4 +1,8 @@
 #ques 1
+def sep(title):
+    print("\n"+"="*20+""+title+""+"="*20)
+
+sep("1st Question")
 import random 
 import string
 
@@ -9,13 +13,14 @@ def generate_password():
 print(generate_password())
 
 #ques 2
-
+sep("2nd Question")
 def calculate_bmi(weight_kg, height_m):
     return weight_kg/(height_m**2)
 
 print("Body Mass Index is ",calculate_bmi(75,1.79))
 
 #ques 3
+sep("3rd Question")
 def book_flight(destination , class_type = 'Economy'):
       return (destination,"confirming booking for ",class_type)
 
@@ -29,7 +34,7 @@ else :
     print(book_flight(destination,class_type))
 
 #ques 4
-
+sep("4th Question")
 def create_profile(username, age, country='Unknown'):
     return{f"username:{username},age:{age}country :{country}"}
 
@@ -45,14 +50,14 @@ else :
 
 
 #Ques 5
-
+sep("5th Question")
 def concatinate_words(*words):
       return "-".join(words)
 
 print(concatinate_words("Manjot ","Sandhu","is","Mad"))
 
 #Ques 6
-
+sep("6th Question")
 def build_configuration(**settings):
       for i,j in settings.items():
        print(f"{i}:{j}")
